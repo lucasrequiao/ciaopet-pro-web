b05bf233dd2bc0d62016c98e9da16c268629aa95
lucas.requiao <lucas.requiao@pm.df.gov.br>
2026-07-09 21:56:31 -0300

    feat: componentes primitivos do design system na galeria

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router"
 import { LoginPage } from "./LoginPage"
 import { DashboardPage } from "./DashboardPage"
+import { GalleryPage } from "./GalleryPage"
 
 export const router = createBrowserRouter([
     {path: "/", element: <DashboardPage />},
     {path: "/login", element: <LoginPage />},
+    {path: "/gallery", element: <GalleryPage />},
 ]) 
